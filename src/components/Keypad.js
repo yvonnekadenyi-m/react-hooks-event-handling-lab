89b458e477d2f5e022abaff051ce3eb3c9f448ce
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-10-27 12:10:42 +0300

    added to components

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
-// Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+import React from "react";
+
+function Keypad() {
+  function handleChange() {
+    console.log("Entering password...");
+  }
+
+  return (
+    <input
+      type="password"
+      onChange={handleChange}
+    />
+  );
 }
 
 export default Keypad;
